8751f947b5f8a2c1fefe3075bed0f10ac6f7a091
Ulf Bengtsson <ulf.bengtsson@lexicon.se>
2019-02-04 08:13:32 +0100

    Add project files.

---
+++ ConsoleOOP_Ex/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleOOP_Ex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
